8cbe2b2de43934aafa8730e5e096c085c73f5700
PaulEastham <pauleastham2@gmail.com>
2026-04-02 19:51:32 +0100

    Update phishing-game.js

--- phishing-game.js
+++ phishing-game.js
@@ -1,3 +1,12 @@
+/* SHUFFLE FUNC */
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+
 /* EMAIL DATA */
 const emails = [
   {
