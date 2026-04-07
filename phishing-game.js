86845e96dce08b821d895e5a165b425db7cbb19d
PaulEastham <pauleastham2@gmail.com>
2026-04-07 23:00:23 +0100

    Update phishing-game.js

--- phishing-game.js
+++ phishing-game.js
@@ -229,7 +229,7 @@ document.getElementById("end-btn").onclick = () => {
 
   showScreen("home");
 };
-shuffle(emails);
+
 
 
 //--------------------------------------------------- POLICE ALERT ---------------------------------------------------------------------//
